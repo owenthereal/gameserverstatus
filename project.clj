35970c1e56557b8e13456bf2471e4459b6e3475f
Jingwen Owen Ou <jingweno@gmail.com>
2012-05-28 16:38:10 -0700

    Add endpoint to return d3 server json

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject gameserverstatus "1.0.0-SNAPSHOT"
             :description "FIXME: write description"
             :dependencies [[org.clojure/clojure "1.4.0"]
+                           [org.clojure/data.json "0.1.3"]
                            [aleph "0.3.0-alpha2"]
                            [enlive "1.0.0"]]
             :main gameserverstatus.main)
--- src/gameserverstatus/main.clj
+++ src/gameserverstatus/main.clj
@@ -1,14 +1,15 @@
 (ns gameserverstatus.main
   (:use [lamina core])
   (:use [aleph http])
+  (:use [clojure.data [json :only [json-str]]])
   (:require [gameserverstatus.games.d3 :as d3]))
 
 (defn hello-world [channel request]
   (enqueue channel
            {:status 200
-            :headers {"content-type" "text/html"}
-            :body "Hello World!"}))
+            :headers {"content-type" "application/json"}
+            :body (json-str (d3/fetch-status))}))
 
 (defn -main []
-  (let [port (Integer/parseInt (or (System/getenv "PORT") 8080))]
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
     (start-http-server hello-world {:port port})))
