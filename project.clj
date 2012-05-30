8b1edea8bd86e95bed3547d3b9469ab69008f263
Jingwen Owen Ou <jingweno@gmail.com>
2012-05-29 17:13:35 -0700

    Add mock controller

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject gameserverstatus "1.0.0-SNAPSHOT"
             :description "FIXME: write description"
             :dependencies [[org.clojure/clojure "1.4.0"]
-                           [org.clojure/data.json "0.1.3"]
                            [noir "1.2.2"]
                            [enlive "1.0.0"]]
             :main gameserverstatus.server)
--- src/gameserverstatus/views/index.clj
+++ src/gameserverstatus/views/index.clj
@@ -1,12 +1,26 @@
 (ns gameserverstatus.views.index
   (:require [gameserverstatus.views.common :as common])
+  (:require [noir.response :as response])
   (:use [noir.core :only [defpage]]
         [hiccup.core :only [html]]))
 
-(defpage "/my-page" []
-  (html
-    [:h1 "This is my first page!"]))
+(def games
+  (list {:id 1, :name "Diablo 3"}
+        {:id 2, :name "War of Warcraft"}))
+
+(def servers
+  {"1" {:id 1, :game-id 1, :name "Game Server (America)", :status "Up"}
+   "2" {:id 2, :game-id 1, :name "Game Server (Europe)", :status "Down"}
+   "3" {:id 3, :game-id 1, :name "Game Server (Asia)", :status "Unknown"}})
 
 (defpage "/" []
          (common/layout
            [:p "Welcome to gameserverstatus"]))
+
+(defpage "/games" []
+         (response/json games))
+
+(defpage "/servers/:id" {server-id :id}
+         (if-let [server (servers server-id)]
+           (response/json (servers server-id))
+           (response/empty)))
